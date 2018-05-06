fea3269e5c01e1d5c5475de2bdcaffbe61e8cfb4
helloforrestworld <dongweijia238@gmail.com>
2018-05-06 19:39:15 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+
+import 'vuetify/dist/vuetify.min.css' 
+
+Vue.config.productionTip = false
+Vue.use(Vuetify)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
