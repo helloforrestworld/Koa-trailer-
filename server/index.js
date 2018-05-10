84d1e3d7e6e6857eb99bcf402a1a019a963fb695
helloforrestworld <dongweijia238@gmail.com>
2018-05-10 08:43:37 +0800

    feat: koa2服务器初始化

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const koa = require('koa')
+const app = new koa()
+const logger = require('koa-logger')
+
+app.use(logger())
+
+app.use(async (ctx, next) => {
+  ctx.body = '电影首页'
+})
+
+app.listen(3333, (err) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('Test Server Running at port 3333')
+  }
+})
